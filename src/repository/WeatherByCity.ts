d39f3b091f53bcf8b45e8e8d837fa4e4717473e2
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-04 16:55:54 +0200

    update WeatherByCity.ts

--- src/repository/WeatherByCity.ts
+++ src/repository/WeatherByCity.ts
@@ -1,26 +1,45 @@
 import CoordinatesByLocation from "./CoordinatesByLocation";
 
 export default class WeatherByCity {
-    private _appId: string;
-    private _lon: number = 0;
-    private _lat: number = 0;
-    private coorindateByLocation:CoordinatesByLocation;
+  private _appId: string;
+  private _lon: number = 0;
+  private _lat: number = 0;
+  private coorindateByLocation: CoordinatesByLocation;
 
-    constructor(appId:string){
-        this._appId = appId;
-        this.coorindateByLocation = new CoordinatesByLocation();
+  constructor(appId: string) {
+    this._appId = appId;
+    this.coorindateByLocation = new CoordinatesByLocation(this._appId);
+  }
+
+  private getUrl(): string {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${this._lat}&lon=${this._lon}&appid=${this._appId}`;
+  }
+  async getWeather(city) {
+    if (city && city === "") {
+      console.error("");
+      return;
     }
-
-    private getUrl(): string {
-        return `https://api.openweathermap.org/data/2.5/weather?lat=${this._lat}&lon=${this._lon}&appid=${this._appId}`;
-      }
-    async getWeather(city){
-
+  }
+  private async getWeahterByCoordinate(): Promise<any> {
+    if (this._lat === "" || this._lon === "") {
+      console.error("");
+      return;
     }
-    private async getWeahterByCoordinate(){
+    const url = this.getUrl();
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
+    try {
+      const response = await fetch(url, options);
+      const jsonData = await response.json();
+      return jsonData;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-
-
-
+  }
 }
