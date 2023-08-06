df67ca16817ff17225e0430e93a96c6a336ac5c1
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-06 21:07:46 +0200

    update dark mode

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -3,12 +3,12 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 type Theme = 'light' | 'dark';
 
 interface ThemeContextProps {
-  theme: Theme;
-  toggleTheme: () => void;
+    theme: Theme;
+    toggleTheme: () => void;
 }
 
-interface ThemeProviderProps{
-    children:React.ReactNode;
+interface ThemeProviderProps {
+    children: React.ReactNode;
 }
 
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
@@ -16,28 +16,41 @@ const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const [theme, setTheme] = useState<Theme>('light');
+    const [theme, setTheme] = useState<Theme>('light');
 
-  const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
+    const toggleTheme = () => {
+        setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
 
-  useEffect(() => {
-    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    setTheme(systemTheme);
-  }, []);
+    useEffect(() => {
+        const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        setTheme(systemTheme);
+    }, []);
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+
+    // add dark class to body (extern to context)
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.body.classList.add('dark');
+            console.log("add dark");
+        } else {
+            document.body.classList.remove('dark');
+            console.log("remove dark");
+
+        }
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
 };
 
 export const useTheme = () => {
-  const context = useContext(ThemeContext);
-  if (!context) {
-    throw new Error('useTheme must be used within a ThemeProvider');
-  }
-  return context;
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+    return context;
 };
