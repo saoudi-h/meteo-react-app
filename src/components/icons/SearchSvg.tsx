1776bfe0f2c1b159dfddadfb08cf694db1ca1ea4
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-06 23:08:30 +0200

    add logos svg tsx

---
+++ src/components/icons/SearchSvg.tsx
@@ -0,0 +1,17 @@
+function SearchSvg() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <g>
+                <path fill="currentColor" d="M0 0H24V24H0z"></path>
+                <path
+                    fill="currentColor"
+                    fillRule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12zm7-.5a2.5 2.5 0 115 0 2.5 2.5 0 01-5 0zM11.5 7a4.5 4.5 0 102.396 8.31l1.397 1.397a1 1 0 001.414-1.414l-1.397-1.397A4.5 4.5 0 0011.5 7z"
+                    clipRule="evenodd"
+                ></path>
+            </g>
+        </svg>
+    );
+}
+
+export default SearchSvg;
---
+++ src/components/icons/XCircleClose.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function XCircleClose() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <g>
+        <path fill="#fff" d="M0 0H24V24H0z"></path>
+        <path
+          fill="#323232"
+          fillRule="evenodd"
+          d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12zm7.707-3.707a1 1 0 00-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 101.414 1.414L12 13.414l2.293 2.293a1 1 0 001.414-1.414L13.414 12l2.293-2.293a1 1 0 00-1.414-1.414L12 10.586 9.707 8.293z"
+          clipRule="evenodd"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default XCircleClose;
