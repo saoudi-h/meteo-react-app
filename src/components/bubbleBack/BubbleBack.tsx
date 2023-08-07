4b9550da1050ed1b0770dfa9f4baa257063150df
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-07 18:04:48 +0200

    add BubbleBack component animation to home

---
+++ src/components/bubbleBack/BubbleBack.tsx
@@ -0,0 +1,72 @@
+import './BubbleBack.sass'
+import React, { useEffect } from 'react';
+import useMeasure from 'react-use-measure';
+import { useTrail, animated } from '@react-spring/web';
+
+const bubbleList = [
+    {
+        constConfig: {
+            diameter: "300px",
+            color: "#90e0ef",
+        },
+        xy: [700, 0],
+        config: { mass: 10, tension: 50, friction: 50 },
+    },
+    {
+        constConfig: {
+            diameter: "150px",
+            color: "#a637cc",
+        },
+        xy: [200, 0],
+        config: { mass: 10, tension: 60, friction: 50 },
+    },
+];
+
+const trans = (x: number, y: number) =>
+    `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`;
+
+interface BubbleBackProps {
+    mouseEvent: React.MouseEvent | undefined;
+}
+const BubbleBack: React.FC<BubbleBackProps> = ({ mouseEvent }) => {
+    const [trail, api] = useTrail(bubbleList.length, i => ({
+        xy: bubbleList[i].xy,
+        config: bubbleList[i].config,
+    }));
+    const [ref, { left, top }] = useMeasure();
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+        api.start({ xy: [e.clientX - left, e.clientY - top] });
+    };
+
+    useEffect(() => {
+        if (mouseEvent)
+            handleMouseMove(mouseEvent);
+    }, [mouseEvent])
+
+
+    return (
+        <div className="bubble">
+            <svg style={{ position: 'absolute', width: 0, height: 0 }}>
+                <filter id="goo">
+                    <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
+                </filter>
+            </svg>
+            <div ref={ref} className="bubble__container">
+                {trail.map((props, index) => (
+                    <animated.div
+                        key={index}
+                        style={{
+                            transform: props.xy.to(trans),
+                            width: bubbleList[index].constConfig.diameter,
+                            height: bubbleList[index].constConfig.diameter,
+                            backgroundColor: bubbleList[index].constConfig.color,
+                        }}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default BubbleBack;
