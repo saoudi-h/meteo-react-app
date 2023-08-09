96a020dc33e9d92e133860b832a11a3680853043
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-09 16:47:57 +0200

    add layout to weather page

--- src/pages/weatherPage/WeatherPage.tsx
+++ src/pages/weatherPage/WeatherPage.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import './WeaterPage.css'
+import './WeaterPage.sass'
 import FormWeather from '../../components/formWeather/FormWeather'
 import WeatherByCity from '../../repository/WeatherByCity'
 import CityCardWeather from '../../components/cityCardWeather/CityCardWeather'
+import DefaultLayout from '../../layout/default/DefaultLayout'
+import { MetaProps } from '../../components/utility/Meta'
 
 const WeatherPage = () => {
+  const metaProps: MetaProps = {
+    title: 'Météo',
+    description: 'Météo par ville',
+    ogSrc: '/og/og_image.png',
+    keyWord: 'weather, MyWeather, météo, prévisions, application'
+  }
   const [city, setCity] = useState<string>('')
   const [result, setResult] = useState<any>()
   const tokenApi = process.env.REACT_APP_WEATHER_TOKEN || ''
@@ -23,12 +31,12 @@ const WeatherPage = () => {
   }
 
   return (
-    <>
+    <DefaultLayout className="weather" metaProps={metaProps}>
       <FormWeather onSubmit={handleSubmit} />
       <div>
         <CityCardWeather weatherData={result} />
       </div>
-    </>
+    </DefaultLayout>
   )
 }
 
