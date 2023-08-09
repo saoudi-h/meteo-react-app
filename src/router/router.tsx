043aea2d8a85119c17f924069a0de3e5117686f4
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-10 00:15:09 +0200

    update router with nested routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,15 +5,17 @@ import WeatherPage from '../pages/weatherPage/WeatherPage'
 import WeatherCityPage from '../pages/weatherCityPage/WeatherCityPage'
 import NotFound from '../pages/notFound/NotFound'
 import About from '../pages/about/About'
+import DefaultLayout from '../layout/default/DefaultLayout'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Home />} />
-      <Route path="weather" element={<WeatherPage />} />
-      <Route path="about" element={<About />} />
-      <Route path="weather/:city" element={<WeatherCityPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="weather" element={<WeatherPage />} />
+        <Route path="about" element={<About />} />
+        <Route path="weather/:city" element={<WeatherCityPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </>
   )
 )
