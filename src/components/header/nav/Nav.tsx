c166959f84342484b61d50de667e9825cdacd5e8
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-15 12:04:51 +0200

    consideration of unlisted pages in Nav

--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './Nav.sass'
-import { Link, useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { useSpring, animated } from '@react-spring/web'
 
 interface NavItem {
@@ -9,25 +9,48 @@ interface NavItem {
 }
 
 const Nav: React.FC = () => {
-  const navItems: NavItem[] = [
+  const location = useLocation()
+  const defaultNavItems = [
     { path: '/', name: 'Accueil' },
     { path: '/weather', name: 'Météo' },
     { path: '/about', name: 'À propos' }
   ]
+  const [navItems, setNavItems] = useState<NavItem[]>([...defaultNavItems])
+
+  const genItemPosition = () => {
+    const newItemPositions: { [key: string]: number } = {}
+    navItems.forEach((item, index) => {
+      newItemPositions[item.path] = index
+    })
+    return newItemPositions
+  }
+
+  const [itemPositions, setItemPositions] = useState<{ [key: string]: number }>(genItemPosition())
+
+  const [notNavItems, setNotNavItems] = useState<NavItem[]>([{ path: '/credits', name: 'crédits' }])
 
   const itemLength = 100
   const marginSwitch = 3
-  const nmbrItems = navItems.length
-  const itemPositions: { [key: string]: number } = {}
-  navItems.forEach((item, index) => {
-    itemPositions[item.path] = index
-  })
-  const location = useLocation()
+  const nmbrItems = () => navItems.length
+
+  useEffect(() => {
+    setItemPositions(genItemPosition())
+  }, [navItems])
+
   const [selectedItem, setSelectedItem] = useState<string>('/')
-  const getLeftValue = (add: number = 0) =>
-    itemPositions[selectedItem] * itemLength + marginSwitch + add + 'px'
-  const getRightValue = (add: number = 0) =>
-    (nmbrItems - 1 - itemPositions[selectedItem]) * itemLength + marginSwitch + add + 'px'
+
+  const getLeftValue = (add: number = 0) => {
+    const selectedPosition = itemPositions[selectedItem] || 0
+    const left = selectedPosition * itemLength + marginSwitch + add + 'px'
+    return left
+  }
+
+  const getRightValue = (add: number = 0) => {
+    const selectedPosition = itemPositions[selectedItem] || 0
+    const right = (nmbrItems() - 1 - selectedPosition) * itemLength + marginSwitch + add + 'px'
+    return right
+  }
+
   const getBorderRadius = () => {
     const borderRadiusLeft = itemPositions[selectedItem] === 0 ? '15px' : '3px'
     const borderRadiusRight = itemPositions[selectedItem] === navItems.length - 1 ? '15px' : '3px'
@@ -35,7 +58,16 @@ const Nav: React.FC = () => {
   }
 
   useEffect(() => {
-    setSelectedItem(location.pathname)
+    if (itemPositions[location.pathname] === undefined) {
+      const unlistedItem = notNavItems.find(
+        (notNavItem) => notNavItem.path === location.pathname
+      ) || { path: '/notfound', name: 'Introuveable' }
+      setSelectedItem(unlistedItem.path)
+      setNavItems([...navItems, { ...unlistedItem }])
+    } else {
+      setSelectedItem(location.pathname)
+      removeUnlistedItems()
+    }
   }, [location])
 
   const [springs, api] = useSpring(() => ({
@@ -52,7 +84,7 @@ const Nav: React.FC = () => {
       right: getRightValue(),
       borderRadius: getBorderRadius()
     })
-  }, [location, selectedItem])
+  }, [location, selectedItem, itemPositions])
 
   const handleItemClick = (clickedPath: string) => {
     if (clickedPath !== selectedItem) {
@@ -77,12 +109,16 @@ const Nav: React.FC = () => {
     })
   }
 
+  const removeUnlistedItems = () => {
+    setNavItems([...defaultNavItems])
+  }
+
   return (
     <ul className="nav-toggle" id="nav-toggle">
       <animated.div className="toggle-item__switch" style={{ ...springs }} />
       {navItems.map((item) => (
-        <li key={item.path} className={`${selectedItem === item.path ? 'selected' : ''}`}>
-          <Link
+        <li key={item.path}>
+          <NavLink
             to={item.path}
             className="link"
             onClick={() => handleItemClick(item.path)}
@@ -90,7 +126,7 @@ const Nav: React.FC = () => {
             onMouseLeave={handleItemHoverEnd}
           >
             <div>{item.name}</div>
-          </Link>
+          </NavLink>
         </li>
       ))}
     </ul>
