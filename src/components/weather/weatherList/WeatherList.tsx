832c34000569baee2b7d05b838b16b254783470a
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-19 22:52:31 +0200

    update WeatherList

--- src/components/weather/weatherList/WeatherList.tsx
+++ src/components/weather/weatherList/WeatherList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import WeatherCard from '../weatherCard/WeatherCard'
 import { RootState } from '../../../store/store'
+import './WeatherList.sass'
 
 const WeatherList = () => {
   const weatherDataList = useSelector(
@@ -9,10 +10,12 @@ const WeatherList = () => {
   )
 
   return (
-    <div>
-      {weatherDataList.map((weatherData, index) => (
-        <WeatherCard key={index} weatherData={weatherData} />
-      ))}
+    <div className="container">
+      <div className="weather-list">
+        {weatherDataList.map((weatherData, index) => (
+          <WeatherCard key={index} weatherData={weatherData} />
+        ))}
+      </div>
     </div>
   )
 }
