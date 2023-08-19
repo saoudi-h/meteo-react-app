3443a03b62b6e2887a3bc77b6e38e3d1b5610ed0
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-19 22:58:30 +0200

    cleanup weatherPage.tsx

--- src/pages/weatherPage/WeatherPage.tsx
+++ src/pages/weatherPage/WeatherPage.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './WeaterPage.sass'
-import FormWeather from '../../components/formWeather/FormWeather'
-import WeatherByCity from '../../repository/WeatherByCity'
-import CityCardWeather from '../../components/cityCardWeather/CityCardWeather'
 import Meta, { MetaProps } from '../../components/utility/Meta'
 import Weather from '../../components/weather/Weather'
 
@@ -13,31 +10,11 @@ const WeatherPage = () => {
     ogSrc: '/og/og_image.png',
     keyWord: 'weather, MyWeather, météo, prévisions, application'
   }
-  const [city, setCity] = useState<string>('')
-  const [result, setResult] = useState<any>()
-  const tokenApi = process.env.REACT_APP_WEATHER_TOKEN || ''
-  const weatherByCity: WeatherByCity = new WeatherByCity(tokenApi)
-  useEffect(() => {
-    if (city !== '') weatherByCity.getWeather(city).then((data) => setResult(data))
-  }, [city])
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget)
-    const searchQuery = formData.get('searchQuery')?.toString() || ''
-    if (searchQuery !== '') {
-      setCity(searchQuery)
-    }
-  }
 
   return (
     <>
       <Meta {...metaProps} />
       <main className="main weather">
-        {/* <FormWeather onSubmit={handleSubmit} />
-        <div>
-          <CityCardWeather weatherData={result} />
-        </div> */}
         <Weather />
       </main>
     </>
