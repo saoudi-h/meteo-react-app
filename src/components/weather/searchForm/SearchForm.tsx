7c3930dab809ce38b69a8cb2648991d6730c8f30
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-19 22:58:14 +0200

    update SearchForm.tsx

--- src/components/weather/searchForm/SearchForm.tsx
+++ src/components/weather/searchForm/SearchForm.tsx
@@ -1,49 +1,81 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './SearchForm.sass'
 import SearchSvg from '../../icons/SearchSvg'
 import LocationSvg from '../../icons/locationSvg'
+import WeatherAPIService from '../../../services/WeatherAPIService'
+import { useDispatch } from 'react-redux'
+import { addWeatherData } from '../../../store/actions/weatherActions'
+import { WeatherData } from '../../../store/types'
+import UnsplashAPIService from '../../../services/UnsplashAPIService'
 
-interface SearchFormProps {
-  onSearch: (city: string, useGeoLocation: boolean) => void
-}
-
-const SearchForm: React.FC<SearchFormProps> = ({ onSearch }) => {
+type GeolocationStatus = 'prompt' | 'granted' | 'denied'
+const SearchForm: React.FC = () => {
+  const dispatch = useDispatch()
+  const weatherTokenApi = process.env.REACT_APP_WEATHER_TOKEN || ''
+  const unsplashTokenApi = process.env.REACT_APP_UNSPLASH_TOKEN || ''
+  const weatherAPIService: WeatherAPIService = new WeatherAPIService(weatherTokenApi)
+  const unsplashAPIService: UnsplashAPIService = new UnsplashAPIService(unsplashTokenApi)
   const [city, setCity] = useState('')
-  const [useGeoLocation, setUseGeoLocation] = useState(false)
-  const handleSearch = (e: React.FormEvent) => {
+  const [geoLocationStatus, setGeoLocationStatus] = useState<GeolocationStatus>('denied')
+  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    onSearch(city, useGeoLocation)
-  }
-  const handleGeoLocationClick = async () => {
-    if (useGeoLocation) {
-    } else {
-      const response = await window.navigator.permissions.query({ name: 'geolocation' })
-      if (response.state === 'granted' || response.state === 'prompt') {
-        setUseGeoLocation(true)
-      } else {
-        alert('Please grant permission for geolocation to use this feature.')
-      }
+    if (city !== '') {
+      handleWeatherDataSuccess(await weatherAPIService.getWeather(city))
     }
   }
+
+  const getGeoLocalisationStatus = async () => {
+    const response = await window.navigator.permissions.query({ name: 'geolocation' })
+    if (response.state) {
+      setGeoLocationStatus(response.state)
+    }
+  }
+
+  useEffect(() => {
+    getGeoLocalisationStatus()
+  }, [])
+
+  const handleGeoLocationClick = async () => {
+    try {
+      const position = await new Promise<GeolocationPosition>((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+      })
+      const { latitude, longitude } = position.coords
+      handleWeatherDataSuccess(
+        await weatherAPIService.fetchWeatherByCoordinates(latitude, longitude)
+      )
+    } catch (e: any) {
+      alert("Erreur lors de l'obtention de la géolocalisation: " + e.message)
+    }
+  }
+
+  const handleWeatherDataSuccess = async (weatherData: WeatherData) => {
+    weatherData.imageUrl = (await unsplashAPIService.getImageForCity(weatherData.name)) || undefined
+    console.log(weatherData.imageUrl)
+    dispatch(addWeatherData(weatherData))
+  }
+
   return (
-    <form onSubmit={handleSearch} className="search_form">
-      <div className="search_form__search">
-        <button type="submit" className="button-submit">
-          <SearchSvg />
-        </button>
-        <input
-          type="search"
-          name="searchQuery"
-          placeholder="Oyonnax..."
-          aria-label="Cherchez la météo par ville"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button className="button-location" type="button" onClick={handleGeoLocationClick}>
-          <LocationSvg />
-        </button>
-      </div>
-    </form>
+    <div className="container">
+      <form onSubmit={handleSearch} className="search_form">
+        <div className="search_form__search">
+          <button type="submit" className="button-submit">
+            <SearchSvg />
+          </button>
+          <input
+            type="search"
+            name="searchQuery"
+            placeholder="Oyonnax..."
+            aria-label="Cherchez la météo par ville"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button className="button-location" type="button" onClick={handleGeoLocationClick}>
+            <LocationSvg />
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
