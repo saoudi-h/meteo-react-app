a35029bfaf85f925601b66b2cbfae075698694ac
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-20 17:42:44 +0200

    update route with index + update DefaultLayout

--- src/layout/default/DefaultLayout.tsx
+++ src/layout/default/DefaultLayout.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import './DefaultLayout.sass'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
-import { Outlet, useLocation } from 'react-router-dom'
-import Home from '../../pages/home/Home'
+import { Outlet } from 'react-router-dom'
 
 const DefaultLayout: React.FC = () => {
-  const isHome = useLocation().pathname === '/'
-
   return (
     <>
       <Header />
-      {isHome && <Home />}
       <Outlet />
       <Footer />
     </>
