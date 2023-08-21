3924dfe3d9b92cfaefdbf10074ccff7d1207efd6
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-22 00:26:41 +0200

    add toastContainer to DefaultLayout.tsx

--- src/layout/default/DefaultLayout.tsx
+++ src/layout/default/DefaultLayout.tsx
@@ -3,10 +3,26 @@ import './DefaultLayout.sass'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useTheme } from '../../contexts/ThemeContext'
 
 const DefaultLayout: React.FC = () => {
+  const { theme } = useTheme()
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={theme}
+      />
       <Header />
       <Outlet />
       <Footer />
