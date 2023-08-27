d3b50c10ba02be0b557f557899eeebe74c80638b
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-27 18:00:25 +0200

    add TimeLocationSvg.tsx

---
+++ src/components/icons/TimeLocationSvg.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const TimeLocationSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      width="100%"
+      height="100%"
+      version="1.1"
+      viewBox="0 0 78.34 84.787"
+      xmlSpace="preserve"
+    >
+      <path d="M62.975 15.784C58.006 7.178 49.107 2.04 39.17 2.04S20.334 7.178 15.365 15.784c-4.969 8.606-4.969 18.882 0 27.488l22.284 38.596a1.758 1.758 0 003.042 0l22.284-38.596c4.969-8.606 4.969-18.882 0-27.488zm-3.043 25.731L39.17 77.476 18.408 41.515c-4.333-7.506-4.333-16.468 0-23.974C22.742 10.035 30.503 5.554 39.17 5.554s16.429 4.481 20.762 11.987c4.334 7.506 4.333 16.468 0 23.974z"></path>
+      <path
+        d="M39.17 19.677v9.889h-5.934m23.735 0c0 9.831-7.97 17.801-17.801 17.801s-17.801-7.97-17.801-17.801 7.97-17.801 17.801-17.801 17.801 7.97 17.801 17.801z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="4px"
+      ></path>
+    </svg>
+  )
+}
+
+export default TimeLocationSvg
