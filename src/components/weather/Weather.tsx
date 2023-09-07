2e5833253dd7e453124848fc97aa8dbe2cd4f2a7
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-07 22:28:25 +0200

    update + fix card unmount with key=name

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from 'react'
 import './Weather.sass'
 import SearchForm from './searchForm/SearchForm'
 import WeatherList from './weatherList/WeatherList'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/store'
 
 const Weather: React.FC = () => {
+  const weatherDataList = useSelector(
+    (state: RootState) => state.weatherDataListState.weatherDataList
+  )
+
   const [highlightedCity, setHighlightedCity] = useState<string | null>(null)
+
   useEffect(() => {
     if (highlightedCity) {
       const timeout = setTimeout(() => {
@@ -14,14 +21,11 @@ const Weather: React.FC = () => {
       return () => clearTimeout(timeout)
     }
   }, [highlightedCity])
+
   return (
     <section className="weather">
       <SearchForm setHighlightedCity={setHighlightedCity} />
-      <WeatherList highlightedCity={highlightedCity} />
-      {/* <FormWeather onSubmit={handleSubmit} />
-            <div>
-                <CityCardWeather weatherData={result} />
-            </div> */}
+      <WeatherList weatherDataList={weatherDataList} highlightedCity={highlightedCity} />
     </section>
   )
 }
