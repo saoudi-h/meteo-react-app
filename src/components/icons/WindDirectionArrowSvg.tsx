38bdc9a833ddfbdef029fcfd0eec99a42c1f3497
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-09 18:05:48 +0200

    add WindDirectionArrowSvg.tsx

---
+++ src/components/icons/WindDirectionArrowSvg.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const WindDirectionArrowSvg: React.FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4 12h16m0 0l-4-4m4 4l-4 4"
+      ></path>
+    </svg>
+  )
+}
+
+export default WindDirectionArrowSvg
