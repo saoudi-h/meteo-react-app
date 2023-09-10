2ca9bd70b3a0c832b21cbaeb8d951ab6f68035bf
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-10 23:49:38 +0200

    add RainSvg.tsx

---
+++ src/components/icons/RainSvg.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const RainSvg: React.FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#000" data-name="Layer 1" viewBox="0 0 24 24">
+      <g>
+        <path
+          d="M17 19a3.47 3.47 0 01-6.94 0c0-3.47 3.47-6.08 3.47-6.08S17 15.56 17 19zM22.5 7.35a3.34 3.34 0 11-6.68 0c0-3.34 3.34-5.85 3.34-5.85S22.5 4 22.5 7.35zM8.18 11.52a3.34 3.34 0 01-6.68 0c0-3.34 3.34-5.84 3.34-5.84s3.34 2.5 3.34 5.84z"
+          fill="none"
+          stroke="currentColor"
+          strokeMiterlimit="10"
+          strokeWidth="1.91px"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default RainSvg
