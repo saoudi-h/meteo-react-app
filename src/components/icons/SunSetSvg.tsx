091762a17e543418530d10d7cdef60de5b6c305b
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-11 04:31:12 +0200

    update svgs sunRise and SunSet

--- src/components/icons/SunSetSvg.tsx
+++ src/components/icons/SunSetSvg.tsx
@@ -4,7 +4,7 @@ const SunSetSvg: React.FC = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <g stroke="currentColor" strokeWidth="1.5">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8 22h8M5 19h14M2 16h20"></path>
+        {/* <path strokeLinecap="round" strokeLinejoin="round" d="M8 22h8M5 19h14M2 16h20"></path> */}
         <path d="M12 6a6 6 0 00-4.5 9.969h9A6 6 0 0012 6z"></path>
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6m0 0l2-2m-2 2l-2-2"></path>
         <path
