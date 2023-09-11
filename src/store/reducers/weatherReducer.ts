da2f9808f340ac42c37f145959282fc7c0100ac4
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-11 23:56:18 +0200

    add Update to weatherList reducer

--- src/store/reducers/weatherReducer.ts
+++ src/store/reducers/weatherReducer.ts
@@ -15,6 +15,36 @@ const weatherReducer = (state = initialState, action: WeatherActionTypes): Weath
         ...state,
         weatherDataList: [...state.weatherDataList, action.payload]
       }
+
+    case 'weatherData/update':
+      if (
+        state.weatherDataList.some((weatherData) =>
+          action.payload.searchMethod === 'city'
+            ? weatherData.coord.lat === action.payload.coord.lat &&
+              weatherData.coord.lon === action.payload.coord.lon
+            : weatherData.searchMethod === 'geolocation'
+        )
+      ) {
+        const updatedList = state.weatherDataList.map((item) => {
+          if (
+            action.payload.searchMethod === 'city'
+              ? item.coord.lat === action.payload.coord.lat &&
+                item.coord.lon === action.payload.coord.lon
+              : item.searchMethod === 'geolocation'
+          ) {
+            return { ...item, ...action.payload }
+          }
+          return item
+        })
+
+        return {
+          ...state,
+          weatherDataList: updatedList
+        }
+      }
+      console.log('je suis else')
+      return state
+
     case 'weatherData/remove':
       return {
         ...state,
