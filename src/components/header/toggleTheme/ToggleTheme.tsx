33630ea77512cf8523d5d922f0366e8513f18992
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-15 17:26:09 +0200

    update ToggleTheme

--- src/components/header/toggleTheme/ToggleTheme.tsx
+++ src/components/header/toggleTheme/ToggleTheme.tsx
@@ -4,72 +4,106 @@ import './ToggleTheme.sass'
 import SunSvg from '../../icons/SunSvg'
 import MoonSvg from '../../icons/MoonSvg'
 import { useSpring, animated } from '@react-spring/web'
+import { classNames } from '../../../lib/classnames'
 
 const ToggleTheme: React.FC = () => {
   const { theme, toggleTheme } = useTheme()
   const [isHovered, setIsHovered] = useState(false)
-  const [leftValue, setLeftValue] = useState('')
-  const [rightValue, setRightValue] = useState('')
 
   const [springs, api] = useSpring(() => ({
     from: {
       width: '30px',
-      borderRadius: theme !== 'dark' ? '15px 3px 3px 15px' : '3px 15px 15px 3px'
-    }
+      borderRadius: theme !== 'dark' ? '15px 3px 3px 15px' : '3px 15px 15px 3px',
+      left: theme !== 'dark' ? '3px' : '"42px"',
+      right: theme !== 'dark' ? '39px' : '42px'
+    },
+    config: { mass: 4, tension: 400, friction: 70 }
   }))
 
   useEffect(() => {
-    setLeftValue(theme !== 'dark' ? '3px' : 'unset')
-    setRightValue(theme !== 'dark' ? 'unset' : '3px')
-  }, [theme])
+    api.start({
+      left: theme !== 'dark' ? '3px' : '42px',
+      right: theme !== 'dark' ? '42px' : '3px'
+    })
+  }, [theme, api])
 
-  const handleToggle = () => {
+  const handleToggle = (e: React.MouseEvent) => {
+    const currentButton = e.currentTarget.getAttribute('name')
+    if (
+      (currentButton === 'sun-left' && theme === 'light') ||
+      (currentButton === 'moon-right' && theme === 'dark')
+    )
+      return
+
     const borderRadiusValue = theme === 'dark' ? '15px 3px 3px 15px' : '3px 15px 15px 3px'
 
     api.start({
+      width: '30px',
       borderRadius: borderRadiusValue
     })
     toggleTheme()
   }
 
-  const handleMouseOver = () => {
+  const handleMouseOver = (e: React.MouseEvent) => {
+    const currentButton = e.currentTarget.getAttribute('name')
+    if (
+      (currentButton === 'sun-left' && theme === 'light') ||
+      (currentButton === 'moon-right' && theme === 'dark')
+    )
+      return
+
     if (!isHovered) {
       setIsHovered(true)
-      api.start({
-        width: '40px'
-      })
+      if (theme === 'dark')
+        api.start({
+          width: '40px',
+          left: '32px'
+        })
+      else
+        api.start({
+          width: '40px',
+          right: '32px'
+        })
     }
   }
 
-  const handleMouseLeave = () => {
+  const handleMouseLeave = (e: React.MouseEvent) => {
     setIsHovered(false)
-    api.start({
-      width: '30px'
-    })
+    if (theme === 'dark')
+      api.start({
+        width: '30px',
+        left: '42px'
+      })
+    else
+      api.start({
+        width: '30px',
+        right: '42px'
+      })
   }
 
   return (
     <div className="toggle-theme">
-      <animated.div
-        className="toggle-theme__switch"
-        style={{ left: leftValue, right: rightValue, ...springs }}
-      />
-      <div
-        className="toggle-theme__svg-box"
+      <animated.div className="toggle-theme__switch" style={{ ...springs }} />
+      <button
+        className={classNames('toggle-theme__svg-box', theme === 'light' ? 'selected' : 'active')}
         onMouseOver={handleMouseOver}
         onClick={handleToggle}
         onMouseLeave={handleMouseLeave}
+        id="sun-left"
+        name="sun-left"
       >
         <SunSvg />
-      </div>
-      <div
-        className="toggle-theme__svg-box"
+      </button>
+      <button
+        className={classNames('toggle-theme__svg-box', theme === 'dark' ? 'selected' : 'active')}
         onMouseOver={handleMouseOver}
         onClick={handleToggle}
         onMouseLeave={handleMouseLeave}
+        id="moon-right"
+        name="moon-right"
       >
         <MoonSvg />
-      </div>
+      </button>
     </div>
   )
 }
